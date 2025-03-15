a245ae7982ac9b8f89942d9076e39e1387e8f6ca
Manya Sharma <manya271001@gmail.com>
2025-03-15 23:17:53 +0530

    backend started

---
+++ Data/FullStackDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;   
+using server.Models;
+namespace server.Data
+{
+
+        public class FullStackDbContext : DbContext
+        {
+            public FullStackDbContext(DbContextOptions<FullStackDbContext> options) : base(options) {
+        
+                }
+
+           
+            public DbSet<NewUser> NewUsers { get; set; }
+           
+        
+          }
+}
---
+++ Models/NewUser.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace server.Models
+{
+    [Index(nameof(Email), IsUnique = true)]
+    public class NewUser
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Format")]
+
+
+        public string Email { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+
+        public decimal InitialBalance { get; set; } = 0;
+        public int NumberOfGroups { get; set; } = 0;
+        public bool HasSetup { get; set; } = false;
+    }
+}
